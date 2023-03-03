d26ed63a0d26643dd0f23fd3057580f75efbe106
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:39:40 +0530

    User details model

--- server/models/userDetails.js
+++ server/models/userDetails.js
@@ -1,6 +1,41 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
 
-
+const UserDetails = sequelize.define('user_details', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    education: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    date_of_birth: {
+        type: DataTypes.DATE,
+        allowNull: true
+    },
+    year_of_experience: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    area_of_expertise: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    alternate_mobile: {
+        type: DataTypes.STRING(13),
+        allowNull: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+});
 
 module.exports = UserDetails;
