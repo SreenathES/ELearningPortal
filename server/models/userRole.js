59eb6f9089a828c7e162f443234cb9ea73d0b653
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:38:05 +0530

    Update userRole.js

--- server/models/userRole.js
+++ server/models/userRole.js
@@ -4,6 +4,11 @@ const User = require('./user');
 const Role = require('./role');
 
 const UserRole = sequelize.define('user_role', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
     user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
