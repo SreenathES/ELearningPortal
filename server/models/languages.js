5fef716fa8f206a22af0104385edab9b8cc9b44d
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:01:35 +0530

    Language model

--- server/models/languages.js
+++ server/models/languages.js
@@ -1,6 +1,16 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
+const Language = sequelize.define('course_category', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    language: {
+        type: DataTypes.STRING(40),
+        allowNull: false
+    }
+});
 
-
-module.exports = Languages;
+module.exports = Language;
