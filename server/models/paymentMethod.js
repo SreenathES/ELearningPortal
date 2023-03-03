d68b1137a2c27217e30ec15f8f8338adc5f6ff00
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:06:26 +0530

    Payment method

--- server/models/paymentMethod.js
+++ server/models/paymentMethod.js
@@ -1,6 +1,16 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
-
+const PaymentMethod = sequelize.define('payment_method', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    method: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    }
+});
 
 module.exports = PaymentMethod;
