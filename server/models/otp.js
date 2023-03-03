af2bcdd767e7fed74e52698b95887dcef86567a2
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:06:02 +0530

    OTP model

--- server/models/otp.js
+++ server/models/otp.js
@@ -1,6 +1,33 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
 
-
+const Otp = sequelize.define('otp', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    otp_code: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    purpose: {
+        type: DataTypes.ENUM('Login', 'ForgotPassword'),
+        allowNull: false
+    },
+    expiry: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+});
 
 module.exports = Otp;
