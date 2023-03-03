a5004f0927fb1d500babdc2abb77f60cc7834930
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:56:14 +0530

    Course category model

--- server/models/courseCategory.js
+++ server/models/courseCategory.js
@@ -1,6 +1,16 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
-
+const CourseCategory = sequelize.define('course_category', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    course_category: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    }
+});
 
 module.exports = CourseCategory;
