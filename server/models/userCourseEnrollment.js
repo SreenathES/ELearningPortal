c406918499b6942699739b97903280613b5d7887
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:19:45 +0530

    User course enrollment model

--- server/models/userCourseEnrollment.js
+++ server/models/userCourseEnrollment.js
@@ -1,6 +1,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Course = require('./course');
 
-
+const UserCourseEnrollment = sequelize.define('user_course_enrollment', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    status: {
+        type: DataTypes.ENUM('Enrolled', 'In Progress', 'Completed'),
+        defaultValue: 'Enrolled'
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Course,
+            key: 'id'
+        }
+    }
+});
 
 module.exports = UserCourseEnrollment;
