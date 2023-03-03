3a045ac2c21148f1c03b4b596d4a80ea90aa4ae5
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:16:13 +0530

    User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,37 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
-
+const User = sequelize.define('user', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    first_name: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(30),
+        allowNull: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING(13),
+        allowNull: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1
+    }
+});
 
 module.exports = User;
