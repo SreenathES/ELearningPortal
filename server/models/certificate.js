b8ac8e40be05818665ac8be5175410e1e4c24adc
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:55:17 +0530

    Certificate model

--- server/models/certificate.js
+++ server/models/certificate.js
@@ -1,6 +1,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Course = require('./course');
 
-
+const Certificate = sequelize.define('certificate', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    certificate_link: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Course,
+            key: 'id'
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+});
 
 module.exports = Certificate;
