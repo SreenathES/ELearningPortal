207c1f874fb021b9201ee540499ff118095a4108
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:45:32 +0530

    Cart items model

--- server/models/cartItems.js
+++ server/models/cartItems.js
@@ -1,6 +1,34 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const Cart = require('./cart');
+const Course = require('./course');
 
-
+const CartItems = sequelize.define('cart_items', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    cart_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Cart,
+            key: 'id'
+        }
+    },
+    course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Course,
+            key: 'id'
+        }
+    },
+    amount: {
+        type: DataTypes.DECIMAL(5,2),
+        allowNull: false
+    }
+})
 
 module.exports = CartItems;
