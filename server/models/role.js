d72b561018950562a5e1c728b2a73f96ae4ba311
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:14:39 +0530

    Role  model

--- server/models/role.js
+++ server/models/role.js
@@ -1,6 +1,17 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
 
-
+const Role = sequelize.define('role', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    role: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        unique: true
+    }
+});
 
 module.exports = Role;
