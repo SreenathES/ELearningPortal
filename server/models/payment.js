5a284684145b6dc0e391755689f5d82e9b64388f
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:13:59 +0530

    Payment model

--- server/models/payment.js
+++ server/models/payment.js
@@ -1,6 +1,51 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Cart = require('./cart');
+const PaymentMethod = require('./paymentMethod')
 
-
+const Payment = sequelize.define('payment', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    payment_method: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: PaymentMethod,
+            key: 'id'
+        }
+    },
+    amount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false
+    },
+    transaction_id: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    cart_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Cart,
+            key: 'id'
+        }
+    },
+    status: {
+        type: DataTypes.ENUM('Paid', 'Failed', 'Pending'),
+        defaultValue: 'Pending'
+    }
+});
 
 module.exports = Payment;
