a55e0836eb854b414a42fde121b778c7daf8c639
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:22:11 +0530

    User course feedback model

--- server/models/userCourseFeedback.js
+++ server/models/userCourseFeedback.js
@@ -1,6 +1,42 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Course = require('./course');
 
-
+const UserCourseFeedback = sequelize.define('user_course_feedback', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    feedback: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    reply: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Course,
+            key: 'id'
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
+});
 
 module.exports = UserCourseFeedback;
