0a04f3036ea6efa9571fe9207d27cf4d3e8c5c27
Sreenath <sreenathes2001@gmail.com>
2023-03-03 12:24:57 +0530

    User course progress model

--- server/models/userCourseProgress.js
+++ server/models/userCourseProgress.js
@@ -1,6 +1,43 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Course = require('./course');
+const CourseContent = require('./courseContent');
 
-
+const UserCourseProgress = sequelize.define('user_course_progress', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    course_content_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: CourseContent,
+            key: 'id'
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    course_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Course,
+            key: 'id'
+        }
+    },
+    completed: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
+});
 
 module.exports = UserCourseProgress;
