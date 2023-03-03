d7b9c677fe449eced9b8f874032d4f5f3011521b
Sreenath <sreenathes2001@gmail.com>
2023-03-03 11:36:02 +0530

    User role model

--- server/models/userRole.js
+++ server/models/userRole.js
@@ -1,6 +1,25 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db').sequelize;
+const User = require('./user');
+const Role = require('./role');
 
-
+const UserRole = sequelize.define('user_role', {
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Role,
+            key: 'id'
+        }
+    }
+})
 
 module.exports = UserRole;
