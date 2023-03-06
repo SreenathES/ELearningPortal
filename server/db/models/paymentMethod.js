40010b9112bd988388ed0d9dc18cd6f0c053731c
Sreenath <sreenathes2001@gmail.com>
2023-03-06 13:59:44 +0530

    Update paymentMethod.js
    Imported models required for association.

--- server/db/models/paymentMethod.js
+++ server/db/models/paymentMethod.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Payment = require('./payment');
 
 const PaymentMethod = sequelize.define('payment_method', {
     id: {
