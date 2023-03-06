814f538c6a7de475f581ca394f4665545feea91f
Sreenath <sreenathes2001@gmail.com>
2023-03-06 15:30:57 +0530

    Created routes files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
+
+const userRoutes = require('./routes/user.routes');
+const instructorRoutes = require('./routes/instructor.routes');
+const adminRoutes = require('./routes/admin.routes');
+const studentRoutes = require('./routes/student.routes');
 
 const app = express();
 
-app.use('/', (req, res) => {
-    res.send('myLearn');
-});
+app.use(cors());
+app.use('/', userRoutes);
+app.use('/instructor', instructorRoutes);
+app.use('/admin', adminRoutes);
+app.use('/student', studentRoutes);
 
 app.listen(process.env.DEV_PORT);
