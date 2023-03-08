e2d55cd6eb1279549ba4ffef5534497bd2f4bfdb
Sreenath <sreenathes2001@gmail.com>
2023-03-08 12:35:08 +0530

    Updated loginValidator
    Changed name to authValidator
    Added function to validate student registration data

---
+++ server/validators/authValidator.js
@@ -0,0 +1,35 @@
+const joi = require('joi');
+
+/**
+ * Validates the input data for the login service.
+ * @param {object} data - An object containing user login credentials (email and password).
+ * @returns {object} A Joi validation result object.
+ */
+const validateLoginData = (data) => {
+    const schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    });
+    return schema.validate(data, { abortEarly: false });
+}
+
+/**
+ * Validates the input data for the student registration service.
+ * @param {object} data - An object containing student's first name, last name, email, phone and password.
+ * @returns {object} A Joi validation result object.
+ */
+const validateStudentData = (data) => {
+    const schema = joi.object({
+        firstName: joi.string().required(),
+        lastName: joi.string(),
+        phone: joi.string().trim().length(10).pattern(/^[0-9]+$/),
+        email: joi.string().email().required(),
+        password: joi.string().required()
+    });
+    return schema.validate(data, { abortEarly: false });
+}
+
+module.exports = {
+    validateLoginData,
+    validateStudentData
+};
