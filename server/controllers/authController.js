5deebc42a952670a672b6c2ce573344d078c02ff
Sreenath <sreenathes2001@gmail.com>
2023-03-08 12:34:01 +0530

    Update authController.js
    Created student registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,21 @@ const authService = require('../services/authService');
 const login = async (req, res) => {
     try {
         const serviceResponse = await authService.login(req.body);
+
+        if (!serviceResponse.isValid) {
+            return res.status(serviceResponse.statusCode).json({ errors: serviceResponse.errors });
+        }
+
+        return res.status(serviceResponse.statusCode).json({ result: serviceResponse.result });
+    } catch (err) {
+        return res.status(500).json({ errors: { server: 'Internal server error' } });
+    }
+}
+
+const registerStudent = async (req, res) => {
+    try {
+        const serviceResponse = await authService.registerStudent(req.body);
+
         if (!serviceResponse.isValid) {
             return res.status(serviceResponse.statusCode).json({ errors: serviceResponse.errors });
         }
@@ -14,5 +29,6 @@ const login = async (req, res) => {
 }
 
 module.exports = {
-    login
+    login,
+    registerStudent
 };
