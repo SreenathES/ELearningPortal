440cf90ffe9de39b001400c918161ebfb493b76c
Sreenath <sreenathes2001@gmail.com>
2023-03-08 12:34:13 +0530

    Update user.routes.js
    added student registration route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/api/auth/login', authController.login);
+router.post('/api/auth/student/register', authController.registerStudent)
 
 module.exports = router;
