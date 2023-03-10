3fadd93c85bd69f6fe5b5eb6b4b6d3c5d941fec5
Sreenath <sreenathes2001@gmail.com>
2023-03-10 12:49:44 +0530

    Added login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomeLayout from "./layouts/HomeLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const routes = createBrowserRouter([
 	{
@@ -11,6 +12,10 @@ const routes = createBrowserRouter([
 			{
 				path: '',
 				element: <Home />
+			},
+			{
+				path: 'login',
+				element: <Login />
 			}
 		]
 	}
