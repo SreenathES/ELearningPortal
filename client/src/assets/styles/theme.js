88dd71aeb16cbc7efd29faecd8bb75602c762558
Sreenath <sreenathes2001@gmail.com>
2023-03-10 09:52:18 +0530

    Create theme.js

---
+++ client/src/assets/styles/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#ffffff',
+        },
+        secondary: {
+            main: '#9c27b0',
+        },
+    },
+});
+
+export default theme;
