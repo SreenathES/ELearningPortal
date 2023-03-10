c28110e9ccaa1bb4f705fe1eb13c8a62a5f9befe
Sreenath <sreenathes2001@gmail.com>
2023-03-10 17:12:40 +0530

    Create authValidator.js

---
+++ client/src/utils/validators/authValidator.js
@@ -0,0 +1,111 @@
+import * as yup from 'yup';
+
+// Schema to validate login credentials
+export const loginSchema = yup.object().shape({
+    email: yup
+        .string()
+        .email('Invalid email')
+        .required('Email is required')
+        .matches(
+            /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            'Invalid email'
+        ),
+    password: yup.string().required('Password is required')
+});
+
+// Schema to validate student registration credentials
+export const studentRegistrationSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+        .required('Name field cannot be empty*'),
+    lastName: yup
+        .string()
+        .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+        .required('Name field cannot be empty*'),
+    email: yup
+        .string()
+        .required('Email is required*')
+        .matches(
+            /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+            'Email must be valid'
+        ),
+    phone: yup
+        .string()
+        .required('Phone is required*')
+        .min(10, 'Phone number must be 10 digits')
+        .max(10, 'Phone number must be 10 digits')
+        .matches(
+            /^[6-9]\d{9}$/,
+            'Enter valid phone number'
+        ),
+    password: yup
+        .string()
+        .required('Password cannot be empty*')
+        .matches(
+            /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#.!]).{8,}$/,
+            'Minimum eight characters, at least one letter, one number, and a special character'
+        ),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], "Passwords doesn't match")
+        .required('Confirm password cannot be empty*')
+});
+
+// Schema to validate instructor registration credentials
+export const instructorRegistrationSchema = yup.object().shape({
+    firstName: yup
+        .string()
+        .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+        .required('Name field cannot be empty*'),
+    lastName: yup
+        .string()
+        .matches(/^[A-Za-z ]*$/, 'Please enter valid name')
+        .required('Name field cannot be empty*'),
+    email: yup
+        .string()
+        .required('Email is required*')
+        .matches(/^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/g, 'Email must be valid'),
+    phone: yup
+        .string()
+        .required('Phone is required*')
+        .min(10, 'Phone number must be 10 digits')
+        .max(10, 'Phone number must be 10 digits')
+        .matches(
+            /^[6-9]\d{9}$/,
+            'Enter valid phone number'
+        ),
+    yearOfExperience: yup.number()
+        .positive()
+        .integer('Enter valid year of experience')
+        .min(0, 'Enter valid year of experience')
+        .required('Year of experience cannot be empty*')
+        .typeError('Enter a valid experience')
+        .test('is-number', 'Enter a valid number', (value) => !isNaN(value)),
+    education: yup.string().required('Field required*'),
+    dateOfBirth: yup.date().required('Field required*').transform((value, inputDate) => {
+        if (inputDate.length === 0) {
+            return null;
+        }
+        return value;
+    }).test("Age must be between 18 and 100", function (value) {
+        const today = new Date();
+        const age = today.getFullYear() - value.getFullYear();
+        value.setFullYear(today.getFullYear());
+        if (age >= 18 && age <= 100) {
+            return true;
+        }
+    }),
+    areaOfExpertise: yup.string().required('Field required*'),
+    password: yup
+        .string()
+        .required('Password cannot be empty*')
+        .matches(
+            /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@#.!]).{8,}$/,
+            'Minimum eight characters, at least one letter, one number, and a special character'
+        ),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], "Passwords doesn't match")
+        .required('Confirm password cannot be empty*')
+});
