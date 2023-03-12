374fe49bd357f5cc8ba7f5e53841bbde5bfa8934
Sreenath <sreenathes2001@gmail.com>
2023-03-12 17:44:34 +0530

    Home page design

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function HomeLayout() {
     return (
         <>
             <Header role={'user'} />
-            <Outlet></Outlet>
+            <Outlet />
+            <Footer />
         </>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,44 @@
-import { Box, Button, Grid, Typography } from '@mui/material';
+import { Box, Button, createTheme, CssBaseline, Grid, ThemeProvider, Typography } from '@mui/material';
 import React from 'react';
 import intro_icon from '../assets/images/intro-icon.jpg';
 import VideocamIcon from '@mui/icons-material/VideocamOutlined';
+import { Container } from '@mui/system';
+
+const theme = createTheme();
 
 function Home() {
     return (
-        <Box mt={3} className='vh-80' display={'flex'}>
-            <Grid
-                container
-                spacing={2}
-                sx={{
-                    fontFamily: `Inter, "Open Sans", sans-serif !important`
-                }}
-            >
-                <Grid item md={6} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} width={1}>
-                    <Typography variant='body1' className='light-grey' gutterBottom textAlign={'center'}>Online courses in English.</Typography>
-                    <Typography variant='h3' fontWeight={'bold'} className='light-black' gutterBottom textAlign={'center'}>
-                        Learning Never Exhausts <span className='primary'>The Mind.</span>
-                    </Typography>
-                    <Typography variant='h6' className='light-grey' gutterBottom textAlign={'center'}>
-                        Discover the endless possibilities of knowledge with,
-                        <span className='d-block'>our online learning portal.</span>
-                    </Typography>
-                    <Button variant="contained" color="secondary" startIcon={<VideocamIcon />}>
-                        Courses
-                    </Button>
-                </Grid>
-                <Grid item md={6} display={'flex'} justifyContent={'center'} alignItems={'center'} width={1}>
-                    <img src={intro_icon} alt='MyLearn' />
-                </Grid>
-            </Grid>
-        </Box>
+        <ThemeProvider theme={theme}>
+            <Container component="main">
+                <CssBaseline />
+                <Box mt={3} minHeight={'80vh'} display={'flex'}>
+                    <Grid
+                        container
+                        spacing={2}
+                        sx={{
+                            fontFamily: `Inter, "Open Sans", sans-serif !important`
+                        }}
+                    >
+                        <Grid item md={6} display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} width={1}>
+                            <Typography variant='body1' className='light-grey' gutterBottom textAlign={'center'}>Online courses in English.</Typography>
+                            <Typography variant='h3' fontWeight={'bold'} className='light-black' gutterBottom textAlign={'center'}>
+                                Learning Never Exhausts <span className='primary'>The Mind.</span>
+                            </Typography>
+                            <Typography variant='h6' className='light-grey' gutterBottom textAlign={'center'}>
+                                Discover the endless possibilities of knowledge with,
+                                <span className='d-block'>our online learning portal.</span>
+                            </Typography>
+                            <Button variant="contained" color="secondary" startIcon={<VideocamIcon />}>
+                                Courses
+                            </Button>
+                        </Grid>
+                        <Grid item md={6} display={'flex'} justifyContent={'center'} alignItems={'center'} width={1}>
+                            <img src={intro_icon} alt='MyLearn' />
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </ThemeProvider>
     )
 }
 
