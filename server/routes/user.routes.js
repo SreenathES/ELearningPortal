8ff023ba1d8908ef55465db95fa0157a574512cc
Sreenath <sreenathes2001@gmail.com>
2023-03-12 18:23:23 +0530

    Included middleware to validate login data

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const { validateLogin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/api/auth/login', authController.login);
+router.post('/api/auth/login', validateLogin, authController.login);
 router.post('/api/auth/student/register', authController.registerStudent)
 
 module.exports = router;
