10a4d642cb01634f4434d0132057505a8352c77b
Sreenath <sreenathes2001@gmail.com>
2023-03-16 23:07:43 +0530

    added new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import AdminLayout from "./layouts/AdminLayout";
 import HomeLayout from "./layouts/HomeLayout";
+import InstructorLayout from "./layouts/InstructorLayout";
+import StudentLayout from "./layouts/StudentLayout";
+import AdminHome from "./pages/admin/AdminHome";
 import Home from "./pages/Home";
+import InstructorHome from "./pages/instructor/InstructorHome";
 import InstructorRegister from "./pages/instructor/InstructorRegister";
 import Login from "./pages/Login";
+import OtpPage from "./pages/OtpPage";
 import Page404 from "./pages/Page404";
+import StudentHome from "./pages/student/StudentHome";
 import StudentRegister from "./pages/student/StudentRegister";
+import UnauthorizedAccess from "./pages/UnauthorizedAccess";
+
+const isLoggedIn = 'true';
 
 const routes = createBrowserRouter([
 	{
@@ -38,6 +49,44 @@ const routes = createBrowserRouter([
 				]
 			}
 		]
+	},
+	{
+		path: 'otp',
+		element: <OtpPage />
+	},
+	{
+		path: 'admin',
+		element: <ProtectedRoutes isLoggedIn={isLoggedIn} userRole='admin'><AdminLayout /></ProtectedRoutes>,
+		children: [
+			{
+				path: '',
+				element: <AdminHome />
+			}
+		]
+	},
+	{
+		path: 'student',
+		element: <ProtectedRoutes isLoggedIn={isLoggedIn} userRole='student'><StudentLayout /></ProtectedRoutes>,
+		children: [
+			{
+				path: '',
+				element: <StudentHome />
+			}
+		]
+	},
+	{
+		path: 'instructor',
+		element: <ProtectedRoutes isLoggedIn={isLoggedIn} userRole='instructor'><InstructorLayout /></ProtectedRoutes>,
+		children: [
+			{
+				path: '',
+				element: <InstructorHome />
+			}
+		]
+	},
+	{
+		path: 'unauthorized',
+		element: <UnauthorizedAccess />
 	}
 ]);
 
